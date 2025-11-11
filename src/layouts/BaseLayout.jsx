2142a9cb900907e6fa315ffdf7bcf966f41ecb25
ramadanriz <ramadhanriz57@gmail.com>
2025-11-12 06:06:09 +0700

    remove copyright

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Copyright from "../components/Copyright";
 
 const BaseLayout = () => {
   return (
     <>
-      <Copyright />
       <Outlet />
       <Navbar />
     </>
