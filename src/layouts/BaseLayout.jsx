db49469391ff62901c2015131d117bd2169076c2
ramadanriz <ramadhanriz57@gmail.com>
2025-11-11 20:09:46 +0700

    add copyright

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Copyright from "../components/Copyright";
 
 const BaseLayout = () => {
   return (
     <>
+      <Copyright />
       <Outlet />
       <Navbar />
     </>
